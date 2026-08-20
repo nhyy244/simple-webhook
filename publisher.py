b045726e9513f5191cf6564e7725f174035916fa
nhyy244 <narcys0814@gmail.com>
2026-08-20 21:49:01 +0200

    register-webhook endpoint done

--- publisher.py
+++ publisher.py
@@ -25,7 +25,13 @@ class EventResponse(Event):
     id: UUID
 
 
-class WebhookSubscription(BaseModel):
+class WebhookSubscriptionResponse(BaseModel):
+    url: str
+    events: list[Event]
+    description: str
+
+
+class WebhookSubscriptionRequest(BaseModel):
     url: str
     events: list[Event]
 
@@ -50,35 +56,60 @@ def substract(number_pair: Pair):
     return number_pair.a - number_pair.b
 
 
-@app.post("/register-webhook", response_model=WebhookSubscription)
-def register_webhook(webhook_subscription: WebhookSubscription):
-    bad_events = [
-        event for event in webhook_subscription.events if event.name not in events
-    ]
-    if len(bad_events) > 0:
-        raise HTTPException(
-            status_code=202,
-            detail=f"these events are not supported: {bad_events}. Supported events: {events}",
-        )
+@app.post("/register-webhook", response_model=WebhookSubscriptionResponse)
+def register_webhook(webhook_subscription: WebhookSubscriptionRequest):
+    webhook_subscriptions_file = Path("webhook_subscriptions.txt")
 
-    urls = Path("webhooks.txt")
-    if urls.exists():
-        with open(urls, "r") as file:
-            for line in file:
-                webhook_sub = WebhookSubscription.model_validate_json(line)
-                if webhook_subscription.url == webhook_sub.url:
+    if webhook_subscriptions_file.exists():
+        with open(webhook_subscriptions_file, "r") as file:
+            lines = file.readlines()
+
+        for index, line in enumerate(lines):
+            if not line.strip():
+                continue
+            webhook_sub = WebhookSubscriptionRequest.model_validate_json(line)
+            if webhook_subscription.url == webhook_sub.url:
+                extra_events = []
+                for e in webhook_subscription.events:
+                    if e not in webhook_sub.events:
+                        extra_events.append(e)
+
+                if len(extra_events) > 0:
+                    for event in extra_events:
+                        webhook_sub.events.append(event)
+
+                    # rewrite the whole file with webhook_sub updated
+                    lines[index] = f"{webhook_sub.model_dump_json()}\n"
+                    with open(webhook_subscriptions_file, "w") as file:
+                        file.writelines(lines)
+
+                    return WebhookSubscriptionResponse(
+                        url=webhook_subscription.url,
+                        events=webhook_subscription.events,
+                        description=f"{webhook_subscription.url} updated with extra events: {extra_events}",
+                    )
+                else:
                     raise HTTPException(
                         status_code=202,
-                        detail=f"{webhook_subscription.url} already receiving events",
+                        detail=f"{webhook_subscription.url} already receiving events: {webhook_sub.events}",
                     )
-
-    with open(urls, "a") as file:  # creates webhooks.txt if it doesn't exist
+    with open(
+        webhook_subscriptions_file, "a"
+    ) as file:  # creates webhook_subscriptions.txt if it doesn't exist
         file.write(f"{webhook_subscription.model_dump_json()}\n")
 
-    return WebhookSubscription(
-        url=webhook_subscription.url, events=webhook_subscription.events
+    return WebhookSubscriptionResponse(
+        url=webhook_subscription.url,
+        events=webhook_subscription.events,
+        description=f"Events {webhook_subscription.events} will be now be sent to {
+            webhook_subscription.url
+        }",
     )
 
 
 if __name__ == "__main__":
+    # with open("webhook_subscriptions.txt", "r") as f:
+    #     c = f.read(1)
+    #     print(c)
+    # print(f.read(1))
     uvicorn.run("publisher:app", host="localhost", port=8000, reload=True)
